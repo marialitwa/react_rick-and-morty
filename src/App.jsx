c040900cdd71f6d499128ff1dc9d2e54c979b98a
Maria Litwa <contact@marialitwa.com>
2023-12-11 15:13:35 +0100

    Add flip effect to cards. Wrap cards in flexbox container.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import styled from 'styled-components';
 import Card from './components/Card.jsx';
 import Header from './components/Header.jsx';
 
@@ -7,9 +8,19 @@ function App() {
   return (
     <>
       <Header />
-      <Card />
+      <Main>
+        <Card />
+      </Main>
     </>
   )
 }
 
 export default App
+
+
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
